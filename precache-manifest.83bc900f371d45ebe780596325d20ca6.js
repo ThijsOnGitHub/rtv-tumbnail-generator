bcf12867534a4df4165c12dd9e02873effa74acc
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-08-25 22:28:45 +0200

    Updates

---
+++ precache-manifest.83bc900f371d45ebe780596325d20ca6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "493c52fd7b770743d59c7fff50ca7454",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1423186cd91f3506028d",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "64b91916753ca5b79d14",
+    "url": "/static/js/2.519aae54.chunk.js"
+  },
+  {
+    "revision": "1423186cd91f3506028d",
+    "url": "/static/js/main.bdcb8ea6.chunk.js"
+  },
+  {
+    "revision": "d9dcc4d557e6f2ed3d71",
+    "url": "/static/js/runtime~main.5c046953.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
