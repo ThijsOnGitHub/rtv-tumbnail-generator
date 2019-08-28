41d851858fcfcc40ca25c331923151347a3b949d
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-08-28 17:19:10 +0200

    Updates

---
+++ precache-manifest.5c6b03b22ba1005e60da14f808cb07c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4768498be32a1e87a546edd545bf2e5c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7de960bfec6f08fadaa1",
+    "url": "./static/css/main.9a652a0e.chunk.css"
+  },
+  {
+    "revision": "7f4c78a9f69c7788dc76",
+    "url": "./static/js/2.415192ef.chunk.js"
+  },
+  {
+    "revision": "7de960bfec6f08fadaa1",
+    "url": "./static/js/main.8ff3504f.chunk.js"
+  },
+  {
+    "revision": "c04fbe6fe721d321252c",
+    "url": "./static/js/runtime~main.5915a7b9.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "./static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.48f608179d5cb54f3b9b32a4f52509ca.js"
+  "./precache-manifest.5c6b03b22ba1005e60da14f808cb07c7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
