7753c319bcb1cad0a579b5d1e7ae50e7aceced67
ThijsOnGitHup <47325246+ThijsOnGitHup@users.noreply.github.com>
2019-09-07 13:28:23 +0200

    change check

--- src/TumbnailCanvas.jsx
+++ src/TumbnailCanvas.jsx
@@ -246,19 +246,14 @@ class TumbnailCanvas extends React.Component{
     }
 
     browserGeschikt(){
-        var ua = window.navigator.userAgent;
-        var msie = ua.indexOf("MSIE ");
-
-        if (msie > 0) // If Internet Explorer, return version number
+        if (/*@cc_on!@*/false || !!document.documentMode) // If Internet Explorer, return version number
         {
-            alert(parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))));
             return false
         }
         else  // If another browser, return 0
         {
             console.log(window.navigator.userAgent);
         }
-
         return true
     }
 
